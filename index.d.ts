15c52e41ce9c96c5cc837c53e8d854ed88f40e56
Steen Klingberg <steen@sk-ab.se>
2023-01-10 20:02:45 +0100

    Typescript fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'xml2td';
-declare function xml2td(xml: string, keep_whitespaces: boolean) : any;
+declare function xml2td(xml: string, keep_whitespaces?: boolean) : any;
 export default xml2td;
 declare function td2xml () : any;
 declare function node () : any;
